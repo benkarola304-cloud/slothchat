d94bbdf3b2db67303ee861dfae86f421a44a7130
benkarola304-cloud <benkarola304@gmail.com>
2025-11-28 00:18:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
-const CACHE_NAME = 'sloth-v2';
+const CACHE_NAME = 'sloth-v3-reborn'; // 升级版本号以强制更新
 const FILES = [
-    './', './index.html', './style.css', './app.js', './manifest.json',
+    './',
+    './index.html',
+    './manifest.json',
+    'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-    'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js'
+    'https://unpkg.com/html5-qrcode'
 ];
 
 self.addEventListener('install', e => {
+    // 强制跳过等待，让新版本立即生效
+    self.skipWaiting();
     e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
 
+self.addEventListener('activate', e => {
+    // 清理旧缓存 (sloth-v2, etc.)
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            })
+        ))
+    );
+});
+
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
